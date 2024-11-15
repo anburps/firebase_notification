7515f73dab5546c18af8f41398c0429143869754
anburps <anbalaganrps@gmail.com>
2024-11-15 20:13:11 +0530

    create product

--- src/notifications/views.py
+++ src/notifications/views.py
@@ -24,5 +24,29 @@ def save_fcm_token(request):
 
 
 
+@csrf_exempt
+def create_product(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        product_name = data.get('name')
+        product_price = data.get('price')
+
+        tokens = UserToken.objects.all()
+        if tokens.exists():
+            new_product = Product.objects.create(
+                name=product_name,
+                price=product_price
+            )
+
+            for token in tokens:
+                print(f"Sending notification to token: {token.token}") 
+                send_push_notification(token.token, new_product.name)
+
+            return JsonResponse({'status': 'Product created and notifications sent.'})
+        else:
+            return JsonResponse({'error': 'No FCM token available. Please allow notifications.'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
+
 def index(request):
     return render(request, 'index.html')
